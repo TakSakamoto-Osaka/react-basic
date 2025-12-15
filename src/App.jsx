90f9fb9d09396636ff28052f6105f1e9560af570
坂本 隆幸 <takayuki.sakamoto@daikin.co.jp>
2025-12-15 15:55:35 +0900

    6章 関数コンポーネントを作成しよう

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import { Header } from './components/Header';
+import { MainContent } from './components/MainContent';
+import { Footer} from './components/Footer';
 
 function App() {
   const siteTitle = "Reactの基礎を学ぼう";
@@ -6,27 +9,9 @@ function App() {
 
   return (
     <>
-      <header>
-        <nav>
-          <h1>{siteTitle}</h1>
-          <ul>
-            <li><a href="#">HOME</a></li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <section>
-          <h2>本教材について</h2>
-          <p>本教材では、Reactの基本的な文法や概念を実践的に学びます。</p>
-          <p>各章で段階的に実装を進め、Reactアプリを作っていきます。</p>
-          <p>本教材を通じて、Reactの基本をしっかりと身につけましょう。</p>
-        </section>
-      </main>
-
-      <footer>
-        <p>&copy; {currentYear} {siteTitle}</p>
-      </footer>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
