d1b8768f770541c8e83f51c7416ca8c50287c31a
坂本 隆幸 <takayuki.sakamoto@daikin.co.jp>
2025-12-15 20:07:48 +0900

    8章 条件分岐で表示を切り替える方法を理解しよう

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
   const siteTitle = "Reactの基礎を学ぼう";
   const currentYear = new Date().getFullYear();
 
+  const progress = 40;
+
   return (
     <>
       <Header siteTitle={siteTitle} />
-      <MainContent />
+      <MainContent progress={progress} />
       <Footer siteTitle={siteTitle} currentYear={currentYear} />
     </>
   )
