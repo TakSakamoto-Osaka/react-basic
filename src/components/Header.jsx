a54f2effa2616eb8b8a1f548af5bba3b630ab363
坂本 隆幸 <takayuki.sakamoto@daikin.co.jp>
2025-12-18 15:34:24 +0900

    10章 イベント処理を理解しよう

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 export function Header({ siteTitle = "サイト名未設定" }) {
+    const handleClick = () => {
+        alert("当社では、プログラミング学習者向けにReactを活用した実践的な学習教材をていきょうしています。Reactの基本から応用まで、手を動かしながら学べるコンテンツを通じて、確かなスキル習得を目指せます。");
+    }
+
 
     return(
         <header>
             <nav>
                 <h1>{siteTitle}</h1>
                 <ul>
-                    <li><a href="#">HOME</a></li>
+                    <li><a href="#" onClick={handleClick}>会社概要</a></li>
                 </ul>
             </nav>
         </header>
